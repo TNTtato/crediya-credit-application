6697b12f005a49f3bbf4118e6db9c3de7f674e43
Gustavo <penarandagustavo5@gmail.com>
2025-09-13 19:21:45 -0500

    abstract in steps

--- domain/usecase/src/main/java/com/crediya/usecase/listapplicationsmanualreview/ListApplicationsManualReviewUseCase.java
+++ domain/usecase/src/main/java/com/crediya/usecase/listapplicationsmanualreview/ListApplicationsManualReviewUseCase.java
@@ -27,10 +27,12 @@ public class ListApplicationsManualReviewUseCase {
 
         return stateRepository.findByName(filter)
                 .switchIfEmpty(Mono.error(new NotValidFilterException(filter)))
-                .flatMap(s -> getItemsPageMono(page, size, s, itemsPage));
+                .flatMap(s -> getApplicationReviewItemsPage(page, size, s, itemsPage));
     }
 
-    private Mono<ItemsPage<ApplicationReviewItem>> getItemsPageMono(int page, int size, State s, ItemsPage<ApplicationReviewItem> itemsPage) {
+    private Mono<ItemsPage<ApplicationReviewItem>> getApplicationReviewItemsPage(
+            int page, int size, State s, ItemsPage<ApplicationReviewItem> itemsPage) {
+
         return creditApplicationRepository.findByStateId(s.getStateId(), page, size)
                 .flatMap(r -> {
 
@@ -39,45 +41,60 @@ public class ListApplicationsManualReviewUseCase {
                     itemsPage.setTotalItems(r.getTotalItems());
                     itemsPage.setSize(r.getSize());
 
-                    r.getItems().forEach(i -> {
-                        ApplicationReviewItem item = new ApplicationReviewItem();
+                    r.getItems().forEach(i -> buildApplicationReviewItem(s, itemsPage, i));
 
-                        item.setInstallments(i.getInstallments());
-                        item.setApplicationState(s.getName());
-                        item.setAmount(i.getCreditAmount());
-                        userGateway.getUserByEmail(i.getEmail()).subscribe(
-                                u -> {
-                                    item.setEmail(u.getEmail());
-                                    item.setName(u.getName());
-                                    item.setBaseSalary(u.getBaseSalary());
-                                }
-                        );
-
-                        creditTypeRepository.findById(i.getCreditTypeId()).subscribe(
-                                c -> {
-                                    item.setCreditType(c.getName());
-                                    item.setInterestRate(c.getInterestRate());
-                                }
-                        );
-
-                        stateRepository.findByName(DefaultValues.APPROVED_APPLICATION.getValue()).subscribe(
-                                a -> {
-                                    creditApplicationRepository.findByStateIdAndEmail(s.getStateId(), i.getEmail())
-                                            .collectList()
-                                            .subscribe(
-                                                    list -> {
-                                                        item.setTotalMonthlyDebtApprovedApplications(
-                                                                list.stream().map(CreditApplication::getCreditAmount)
-                                                                        .reduce(0.0, Double::sum)
-                                                        );
-                                                    }
-                                            );
-                                }
-                        );
-
-                        itemsPage.getItems().add(item);
-                    });
                     return Mono.just(itemsPage);
                 });
+    }
+
+    private void buildApplicationReviewItem(State s, ItemsPage<ApplicationReviewItem> itemsPage, CreditApplication i) {
+        ApplicationReviewItem item = new ApplicationReviewItem();
+
+        item.setInstallments(i.getInstallments());
+        item.setApplicationState(s.getName());
+        item.setAmount(i.getCreditAmount());
+        getUserInformationForItem(i, item);
+
+        getCreditTypeInformationForItem(i, item);
+
+        calculateExistingMonthlyTotalDebt(s, i, item);
+
+        itemsPage.getItems().add(item);
+    }
+
+    private void calculateExistingMonthlyTotalDebt(State s, CreditApplication i, ApplicationReviewItem item) {
+        stateRepository.findByName(DefaultValues.APPROVED_APPLICATION.getValue()).subscribe(
+                a -> {
+                    creditApplicationRepository.findByStateIdAndEmail(s.getStateId(), i.getEmail())
+                            .collectList()
+                            .subscribe(
+                                    list -> {
+                                        item.setTotalMonthlyDebtApprovedApplications(
+                                                list.stream().map(CreditApplication::getCreditAmount)
+                                                        .reduce(0.0, Double::sum)
+                                        );
+                                    }
+                            );
+                }
+        );
+    }
+
+    private void getCreditTypeInformationForItem(CreditApplication i, ApplicationReviewItem item) {
+        creditTypeRepository.findById(i.getCreditTypeId()).subscribe(
+                c -> {
+                    item.setCreditType(c.getName());
+                    item.setInterestRate(c.getInterestRate());
+                }
+        );
+    }
+
+    private void getUserInformationForItem(CreditApplication i, ApplicationReviewItem item) {
+        userGateway.getUserByEmail(i.getEmail()).subscribe(
+                u -> {
+                    item.setEmail(u.getEmail());
+                    item.setName(u.getName());
+                    item.setBaseSalary(u.getBaseSalary());
+                }
+        );
     }
 }
